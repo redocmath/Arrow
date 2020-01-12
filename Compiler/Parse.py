9baf3eac43780595d09c6979ed656a343bf67004
CoderLoveMath <59589012+CoderLoveMath@users.noreply.github.com>
2020-01-12 21:14:48 +0900

    Edit Parse.py to compile hello_world example
    ver 0.3

--- Compiler/Parse.py
+++ Compiler/Parse.py
@@ -1,6 +1,6 @@
 var = {}
-func = {}
-run = []
+func = {'print': 'string:out string'}
+run = {}
 errorCode = 0
 
 
@@ -8,64 +8,69 @@ def error(message):
     print("syntax error :", message)
 
 
-with open("file.arrow", 'r') as f:
-    while True:
-        word = f.readline()
-        if not word: break
+def parse(location):
+    error_code = 0
+    with open(location, 'r') as f:
+        while True:
+            word = f.readline()
+            if not word: break
 
-        # print(word)
-        
-        word = word.split(' ')
-        for i in range(len(word)):
-            if '\n' in word[i]:
-                word[i] = word[i][:-1]
-        for i in range(len(word)):
-            try:
-                if word[i] == '=' and word[0] != 'function':
-                    string = word[i + 1]
-                    for j in range(i + 2, len(word) - 1):
-                        if word[j] == '+':
-                            string = string + '+' + word[j + 1]
-                        if word[j] == '-':
-                            string = string + '-' + word[j + 1]
-                        if word[j] == '*':
-                            string = string + '*' + word[j + 1]
-                        if word[j] == '/':
-                            string = string + '/' + word[j + 1]
-                    var[word[i - 1]] = string
-                    break
-                elif word[i] == 'function':
-                    if i == 0 and word[i + 2] == '=' and word[i + 3] == '{' and word[len(word) - 1] == '}':
-                        string = word[4]
-                        for j in range(6, len(word) - 1):
-                            if word[j - 1] == '=>':
-                                string = string + '=>' + word[j]
-                        func[word[i + 1]] = string
-                        break
-                    else:
-                        error('E1: word connected')
-                        errorCode = 1
-                        break
-                elif word[i] == '/-/':
-                    break
-                elif word[i] == 'run':
-                    run.append(word[i + 1])
-                    break
-                elif i != 0:
-                    if errorCode == 0:
-                        error('E0: invalid token')
-                        errorCode = 1
-                        break
-            except:
-                if errorCode == 0:
-                    error('runtime error')
-                    errorCode = 1
-                    break
+            # print(word)
 
-if errorCode == 1:
-    exit('build failed')
-else:
-    print('var:', var)
-    print('func:', func)
-    print('run:', run)
-    exit('build success!')
+            word = word.split(' ')
+            for i in range(len(word)):
+                if '\n' in word[i]:
+                    word[i] = word[i][:-1]
+            for i in range(len(word)):
+                try:
+                    if word[i] == '=' and word[0] != 'function':
+                        string = word[i + 1]
+                        for j in range(i + 2, len(word) - 1):
+                            if word[j] == '+':
+                                string = string + '+' + word[j + 1]
+                            if word[j] == '-':
+                                string = string + '-' + word[j + 1]
+                            if word[j] == '*':
+                                string = string + '*' + word[j + 1]
+                            if word[j] == '/':
+                                string = string + '/' + word[j + 1]
+                        var[word[i - 1]] = string
+                        break
+                    elif word[i] == 'function':
+                        if i == 0 and word[i + 2] == '=' and word[i + 3] == '{' and word[len(word) - 1] == '}':
+                            string = word[4]
+                            for j in range(6, len(word) - 1):
+                                if word[j - 1] == '=>':
+                                    string = string + '=>' + word[j]
+                            func[word[i + 1]] = string
+                            break
+                        else:
+                            error('E1: word connected')
+                            error_code = 1
+                            break
+                    elif word[i] == '/-/':
+                        break
+                    elif word[i] == 'run':
+                        string = word[i + 2]
+
+                        for j in range(i + 3, len(word) - 1):
+                            string = string + ' ' + word[j]
+                        run[word[i + 1]] = string
+                        break
+                    elif i != 0:
+                        if error_code == 0:
+                            error('E0: invalid token')
+                            error_code = 1
+                            break
+                except:
+                    if error_code == 0:
+                        error('runtime error')
+                        error_code = 1
+                        break
+
+    if error_code == 1:
+        exit('build failed')
+    else:
+        print('var:', var)
+        print('func:', func)
+        print('run:', run)
