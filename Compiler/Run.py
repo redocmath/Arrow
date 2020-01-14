e3592f7ebe282e7e90e63ccd011bbca3832f9c3a
CoderLoveMath <59589012+CoderLoveMath@users.noreply.github.com>
2020-01-14 19:44:43 +0900

    Update Run.py

--- Compiler/Run.py
+++ Compiler/Run.py
@@ -1,4 +1,5 @@
 import Parse as parse
+import _debug
 
 
 def func_run():
@@ -6,29 +7,25 @@ def func_run():
     run = list(parse.run.keys())
     var = list(parse.var.values())
     tag = list(parse.var.keys())
-
     value = list(parse.run.values())
     for i in range(len(parse.run)):
         if run[i] in func:
-            temp_for_out_eval = 0
             code = parse.func[run[i]].split(':')
             parameter = code[0].split(',')
             real_parameter = value[i].split(', ')
-            print(real_parameter)
-            print(parameter)
+            if _debug.debug:
+                print(real_parameter)
+                print(parameter)
             fpart = code[1].split('=>')
             for j in range(len(fpart)):
-                if temp_for_out_eval == 1:
-                    break
                 part = fpart[j].split(' ')
-                for k in range(len(func)):
-                    if part[0] == 'out':
-                        if parameter[j] in part[1]:
+                if part[0] == 'out':
+                    if parameter[j] in part[1]:
+                        try:
                             if real_parameter[j][0] == "'" and real_parameter[j][len(real_parameter[j]) - 1] == "'":
                                 print(real_parameter[j][1:-1], end=' ')
                             else:
                                 for q in range(len(parse.var)):
-                                    temp_for_out_eval = 1
                                     if tag[q] in real_parameter:
                                         edit = var[q]
                                         while True:
@@ -39,8 +36,14 @@ def func_run():
                                                     temp = 1
                                             if temp == 0:
                                                 break
-                                        print(eval(edit))
-            print('')
+                                    print(eval(edit), end='')
+                        except:
+                            continue
+                elif part[0] == 'in':
+                    if parameter[j] in part[1]:
+                        memory = "'" + input('please input for ' + real_parameter[j] + ': ') + "'"
+                        parse.var[real_parameter[j]] = memory
+                        var = list(parse.var.values())
 
 
 if __name__ == '__main__':
